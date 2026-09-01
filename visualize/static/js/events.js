2142eae82e981630654ff34327b349bee5c6720e
Ryan Hodges <ryan.d.hodges@gmail.com>
2026-09-01 16:03:23 -0700

    override attribute report field's display_name when attributeInfo provides one

--- visualize/static/js/events.js
+++ visualize/static/js/events.js
@@ -128,8 +128,12 @@ if (!app.wrapper.events.hasOwnProperty('clickOnArcRESTLayerEvent')) {
                     }
                   }
                   if (data && app.utils.trim(data) !== "") {
+                    let display_name = field.alias;
+                    if (attribute_rules.display !== '' && attribute_rules.display !== null) {
+                      display_name = attribute_rules.display;
+                    }
                     attributeObjs.push({
-                      'display': field.alias,
+                      'display': display_name,
                       'data': data
                     });
                   }
